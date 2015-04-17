ce94d419b60235f63de4ee420880ad452d160907
Charles J Hardy <chuckjhardy@gmail.com>
2015-04-17 21:36:55 +0100

    Handle Registration for Submission

--- app/components/__tests__/register-test.js
+++ app/components/__tests__/register-test.js
@@ -5,7 +5,10 @@ jest.dontMock('../register');
 var React = require('react');
 var TestUtils = require('react/lib/ReactTestUtils')
 
-describe('Register', function () {
+var App = require('../../components/app');
+var FirebaseService = require('../../services/firebase_service');
+
+describe('Register', function() {
   var Register = require('../register');
 
   var subject = function() {
@@ -13,6 +16,37 @@ describe('Register', function () {
       <Register />
     );
   };
+
+  beforeEach(function() {
+    FirebaseService.users.create = jest.genMockFunction();
+  });
+
+  describe('#handleSubmit', function() {
+    var adapter = jest.genMockFn();
+    var email = 'test@example.com';
+    var password = 'password';
+
+    it('calls sendToFirebase with expected arguments', function() {
+      App.firebase = jest.genMockFunction().mockReturnValue(adapter);
+
+      var localSubject = subject();
+
+      localSubject.refs.email.getDOMNode().value = email;
+      localSubject.refs.password.getDOMNode().value = password;
+
+      localSubject.handleSubmit({preventDefault: jest.genMockFn()})
+
+      expect(FirebaseService.users.create).toBeCalledWith(
+        adapter,
+        email,
+        password,
+        function() {},
+        function() {},
+        function() {},
+        function() {}
+      );
+    });
+  });
 
   it('renders', function() {
     expect(subject().getDOMNode().textContent)
--- app/components/register.jsx
+++ app/components/register.jsx
@@ -2,24 +2,47 @@
 
 var React = require('react');
 
+var App = require('../components/app');
+var FirebaseService = require('../services/firebase_service');
+
 var logoImage = require('../assets/images/logo-big.png');
 
 module.exports = React.createClass({
   displayName: 'Register',
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    this.sendToFirebase(
+      this.refs.email.getDOMNode().value.trim(),
+      this.refs.password.getDOMNode().value.trim()
+    );
+  },
+  sendToFirebase: function(email, password) {
+    FirebaseService.users.create(
+      App.firebase,
+      email,
+      password,
+      function() {},
+      function() {},
+      function() {},
+      function() {}
+    )
+  },
 
   render: function() {
     return (
       <div>
         <img src={logoImage} alt='' className='hero-logo' />
         <div className='hero-title'>Register</div>
-        <form className='hero-form'>
+        <form className='hero-form' onSubmit={this.handleSubmit}>
           <div className='form-field'>
             <label>Email</label>
-            <input type='text' autofocus required />
+            <input ref='email' type='text' autofocus required />
           </div>
           <div className='form-field'>
             <label>Password</label>
-            <input type='text' required />
+            <input ref='password' type='text' required />
           </div>
           <div className='form-field'>
             <input type='submit' value='Register' className='btn' />
