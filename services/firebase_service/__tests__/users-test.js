fa56e37fbc03589ce07baaeeb2d9e7fec20c9954
Charles J Hardy <chuckjhardy@gmail.com>
2015-04-23 20:24:05 +0100

    Add Remove User Function to Firebase User Service

--- services/firebase_service/__tests__/users-test.js
+++ services/firebase_service/__tests__/users-test.js
@@ -14,6 +14,7 @@ describe('FirebaseService/Users', function() {
     return {
       authWithPassword: function(params, callback) { callback(error, data); },
       createUser: function(params, callback) { callback(error, data); },
+      removeUser: function(params, callback) { callback(error, data); },
       resetPassword: function(params, callback) { callback(error, data); }
     };
   };
@@ -128,6 +129,99 @@ describe('FirebaseService/Users', function() {
       it('calls off to logger with correct args', function() {
         expect(Logger.warn.users.accessDenied)
           .toBeCalledWith(localEmail, password, error, __LOCKDOWN_KEY__);
+      });
+    });
+  });
+
+  describe('#destroy', function() {
+    var asserts = {};
+    var callbacks = {
+      invalidUserCallback: function(email) { asserts['invalidUserCallback'] = email },
+      invalidPasswordCallback: function(email) { asserts['invalidPasswordCallback'] = email },
+      errorCallback: function(error) { asserts['errorCallback'] = error },
+      successCallback: function(data) { asserts['successCallback'] = data },
+    };
+
+    var subject = function(error, data, emailOverride) {
+      return Users.destroy(
+        mockAdapter(error, data),
+        emailOverride || email,
+        password,
+        callbacks.invalidUserCallback,
+        callbacks.invalidPasswordCallback,
+        callbacks.errorCallback,
+        callbacks.successCallback
+      );
+    };
+
+    describe('Success', function() {
+      var data = { uid: 123 };
+
+      beforeEach(function() {
+        Logger.notice.users.destroyed = jest.genMockFunction();
+        subject(null, data)
+      });
+
+      it('calls callback with email', function() {
+        expect(asserts.successCallback).toEqual(data);
+      });
+
+      it('calls off to logger with correct args', function() {
+        expect(Logger.notice.users.destroyed).toBeCalledWith(email, password, data);
+      });
+    });
+
+    describe('Invalid User', function() {
+      var error = { code: 'INVALID_USER' };
+
+      beforeEach(function() {
+        Logger.warn.users.invalidUser = jest.genMockFunction();
+        subject(error);
+      });
+
+      it('calls callback with email', function() {
+        expect(asserts.invalidUserCallback).toEqual(email);
+      });
+
+      it('calls off to logger with correct args', function() {
+        expect(Logger.warn.users.invalidUser)
+          .toBeCalledWith(email, error);
+      });
+    });
+
+    describe('Invalid Password', function() {
+      var error = { code: 'INVALID_PASSWORD' };
+
+      beforeEach(function() {
+        Logger.warn.users.invalidPassword = jest.genMockFunction();
+        subject(error);
+      });
+
+      it('calls callback with email', function() {
+        expect(asserts.invalidPasswordCallback).toEqual(email);
+      });
+
+      it('calls off to logger with correct args', function() {
+        expect(Logger.warn.users.invalidPassword)
+          .toBeCalledWith(email, password, error);
+      });
+    });
+
+    describe('Error', function() {
+      var error = 'Oops';
+
+      beforeEach(function() {
+        Logger.warn.users.destroyFail = jest.genMockFunction();
+        subject(error);
+      });
+
+      it('calls callback with email', function() {
+        expect(asserts.errorCallback).toEqual(error);
+      });
+
+      it('calls off to logger with correct args', function() {
+        expect(Logger.warn.users.destroyFail)
+          .toBeCalledWith(email, password, error);
       });
     });
   });
