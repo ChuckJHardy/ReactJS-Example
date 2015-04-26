734a1e6edb9ffb9fca1b67ebf3240f8fd6e0174e
Charles J Hardy <chuckjhardy@gmail.com>
2015-04-26 20:48:22 +0100

    Add Lodash

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -36,6 +36,9 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      _: 'lodash',
+    }),
     new webpack.DefinePlugin({
       VERSION: JSON.stringify('0.0.1'),
       __GOOGLE_ANALYTICS_KEY__: JSON.stringify(env.GOOGLE_ANALYTICS_KEY),
